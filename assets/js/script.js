28c69d4f935f88cd3641b14a93af4468bf2dbc74
William27310 <william.27@free.fr>
2025-06-30 13:34:34 +0200

    fini

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-let alpha_tab = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+// let alpha_tab = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
 // let stringLetters = "abcdefjhgfxwopdpojdfjpo"
 
@@ -13,11 +13,28 @@ let alpha_tab = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"
 
 // }
 
-const randomWords = [ "table","peripatheticien","pharmacien","tuyau","kayou","jesaispas"]
+let randomWords = [ "table","peripatheticien","pharmacien","tuyau","kayou","jesaispas"]
 console.log(randomWords)
 
-aleaWords = Math.floor(Math.random() * randomWords.length)
+aleaWords = randomWords[Math.floor(Math.random() * randomWords.length)]
+
+let word = aleaWords
+
+console.log(aleaWords)
+// console.log("Le mot est : " + aleaWords)
+// document.getElementById("Word").innerText = aleaWords
+
+for (let i = 0; i < aleaWords.length ; i++) {
+    document.getElementById("guessWord").innerHTML += `
+    <div id="letter-${i}" class="letter mx-1"> _ </div>
+    `
+}
 
 
-console.log("Le mot est : " + aleaWords)
-document.getElementById("Word").innerText = aleaWords
+function guessLetter(letter) {
+    for (let index = 0; index < word.length; index++ ) {
+        if (word[index] == letter.toLowerCase()) {
+            document.getElementById(`letter-${index}`).innerText = letter
+        }
+    }
+}
